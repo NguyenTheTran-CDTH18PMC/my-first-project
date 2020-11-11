826f290923c5cf9c12475f78dba92ce085b61b4f
NguyenTheTran-CDTH18PMC <73585216+NguyenTheTran-CDTH18PMC@users.noreply.github.com>
2020-11-11 13:12:30 +0700

    My first commit

---
+++ Form_DanhSachPhim/app/src/main/java/com/example/form_danhsachphim/fragment1.java
@@ -0,0 +1,26 @@
+package com.example.form_danhsachphim;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+
+public class fragment1<searchView> extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1_layout,container,false);
+
+    }
+
+
+}
